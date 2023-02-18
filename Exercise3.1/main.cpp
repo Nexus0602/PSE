baf852d404d7bae37883dcbdb46f0b10f2954a5c
Nexus0602 <124597301+Nexus0602@users.noreply.github.com>
2023-02-18 20:29:05 +0100

    Update main.cpp

--- Exercise3.1/main.cpp
+++ Exercise3.1/main.cpp
@@ -2,21 +2,47 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
+/** 
+ * @brief 
+ * Function declaration
+ */
 void destroy();
 bool graphicsinitialization(int width, int height);
 void process_input(bool *game_is_running);
 void update();
 void render();
 bool game_running=false;
-int player_x=20;
-int player_y=20;
-int dx = 1;
-int dy = 1;
+
+//int player_x=20;
+//int player_y=20;
+//int dx = 1;
+//int dy = 1;
+
+/** 
+ * @brief 
+ * Global variables
+ */
+struct {
+    int x = 20 ;
+    int y = 20 ;
+} player;
+
+struct{
+    int x = 1 ;
+    int y = 1 ; 
+} d;
+
 SDL_Event sdl_event;
 SDL_Window* window;
 SDL_Renderer* renderer;
 
-
+/** 
+ * @brief 
+ * Main
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char** argv)
 {
     game_running = graphicsinitialization(640,400);
@@ -30,8 +56,11 @@ int main(int argc, char** argv)
     return 0;
 }
 
-
-
+/** 
+ * @brief 
+ * Destroy function
+ * Destroys renderer and window
+ */
 void destroy()
 {
     SDL_DestroyRenderer(renderer);
@@ -39,6 +68,16 @@ void destroy()
     SDL_Quit();
     std::cout << "Graphichs destroyed" << std::endl;
 }
+
+/**
+ * @brief 
+ * Graphic initialization function
+ * Creates window and renderer
+ * @param width 
+ * @param height 
+ * @return true 
+ * @return false 
+ */
 bool graphicsinitialization(int width, int height)
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) !=0)
@@ -78,6 +117,12 @@ bool graphicsinitialization(int width, int height)
     return true;
 }
 
+/**
+ * @brief 
+ * Process input function
+ * Processes the user input
+ * @param game_is_running 
+ */
 void process_input(bool *game_is_running)
 {
     SDL_PollEvent(&sdl_event);
@@ -93,23 +138,35 @@ void process_input(bool *game_is_running)
     }
 
 }
+
+/**
+ * @brief 
+ * Update function
+ * Updates player position (both x and y)
+ */
 void update()
 {
-    if (player_x > 640 - 10 || player_x < 0) {
-        dx = -dx;
+    if (player.x > 640 - 10 || player.x < 0) {
+        d.x = -d.x;
     }
-    if (player_y > 400 - 10 || player_y < 0) {
-        dy = -dy;
+    if (player.y > 400 - 10 || player.y < 0) {
+        d.y = -d.y;
     }
-    player_x += dx;
-    player_y += dy;
+    player.x += d.x;
+    player.y += d.y;
 }
+
+/**
+ * @brief
+ * Render function
+ * Defines and updates variables related to render
+ */
 void render()
 {
     SDL_SetRenderDrawColor(renderer,30,30,30,255);
     SDL_RenderClear(renderer);
 
-    SDL_Rect box = {player_x, player_y, 10,10};
+    SDL_Rect box = {player.x, player.y, 10,10};
     SDL_SetRenderDrawColor(renderer,255,255,255,255);
     SDL_RenderFillRect(renderer, &box);
 
