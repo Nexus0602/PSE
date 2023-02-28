fae383c56cc50daafc891a0707f9aa9591d57c1f
Nexus0602 <124597301+Nexus0602@users.noreply.github.com>
2023-02-28 16:48:34 +0100

    Add files via upload

---
+++ ENGINE/Vec2.cpp
@@ -0,0 +1,142 @@
+#include "Vec2.hh"
+#include <math.h>
+#include <iostream>
+#include <cmath>
+
+
+Vec2::Vec2() : x(0.0), y(0.0) {}
+
+Vec2::Vec2(float x, float y) : x(x), y(y) {}
+
+
+void Vec2::SetVec(float x, float y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+void Vec2::Add(const Vec2 &v)
+{
+    x += v.x;
+    y += v.y;
+}
+void Vec2::Sub(const Vec2 &v)
+{
+    x -= v.x;
+    y -= v.y;
+}
+void Vec2::Scale(const float n)
+{
+    x *= n;
+    y *= n;
+}
+
+void Vec2::Scale(const Vec2 &v)
+{
+    x *= v.x;
+    y *= v.y;
+}
+
+Vec2 Vec2::Rotate(const float angle) const
+{
+	float rads = (angle * M_PI) / 180; // deg to rad
+    return Vec2(x * cosf(rads) - y * sinf(rads),
+    x * cosf(rads) + y * sinf(rads) );
+}
+
+float Vec2::Magnitude() const{
+
+    return sqrtf(x*x + y*y);
+
+}
+float Vec2::MagnitudeSquared() const{
+    return (x*x + y*y);
+
+}
+float Vec2::Angle(){
+    //angulo vector respecto eje horizontal
+    return atan2(y,x) * 180.0 / M_PI;
+}
+
+float Vec2::AngleBetween(const Vec2& v){
+    //angulo entre 2 vec
+    return atan2(v.y - y, v.x - x) * 180.0 / M_PI;
+}
+
+Vec2 Vec2::UnitVec() const{
+
+    return Vec2( x/Magnitude(),  y/Magnitude());
+
+}
+
+Vec2& Vec2::Normalize(){
+
+    float a = x;
+    float b = y;
+
+    a /=Magnitude();
+    b /= Magnitude();
+
+    x = a;
+    y = b;
+
+    return *this;
+}
+
+Vec2 Vec2::Normal() const{
+    // vector perpendicular
+    return Vec2(-x,y);
+
+}
+
+float Vec2::Dot(const Vec2& v) const{
+    return (x * v.x + y * v.y);
+
+}
+float Vec2::Cross(const Vec2& v) const{
+    return (x * v.x - y * v.y);
+
+}
+
+// OPERADORES
+
+bool Vec2::operator==(const Vec2& v) const{
+    if (x == v.x && y == v.y)
+        return true;
+    else
+        return false;
+}
+
+bool Vec2::operator!=(const Vec2& v) const{
+    if (x == v.x && y == v.y)
+        return false;
+    else
+        return true;
+}
+
+Vec2& Vec2::operator += (const Vec2& v){
+    x += v.x;
+    y += v.y;
+    return *this;
+
+}
+Vec2& Vec2::operator -= (const Vec2& v){
+    x -= v.x;
+    y -= v.y;
+    return *this;
+
+}
+Vec2& Vec2::operator *= (const float n){
+    x *= n;
+    y *= n;
+    return *this;
+
+}
+Vec2& Vec2::operator /= (const float n){
+    x /= n;
+    y /= n;
+    return *this;
+
+}
+
+
