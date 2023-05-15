8a26137effe3c76488dd33a92bcf16fe766adf5c
Nexus0602 <124597301+Nexus0602@users.noreply.github.com>
2023-05-15 11:23:26 +0200

    Delete FINAL PROJECT/ECS directory

--- FINAL PROJECT/ECS/Components/GridMovement.hh
+++
@@ -1,6 +0,0 @@
-struct GridMovementComponent
-{   
-    int stepSize;
-
-    GridMovementComponent(int stepSize = 0): stepSize(stepSize) {}
-};
--- FINAL PROJECT/ECS/Components/Sound.hh
+++
@@ -1,8 +0,0 @@
-#include <SDL2/SDL_mixer.h>
-
-struct SoundComponent {
-    Mix_Chunk* soundEffect;
-    bool shouldPlay;
-
-    SoundComponent(Mix_Chunk* soundEffect, bool shouldPlay = false ) : soundEffect(soundEffect), shouldPlay(shouldPlay) {}
-};
