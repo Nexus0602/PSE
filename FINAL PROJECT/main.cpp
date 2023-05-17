b83dfd52c257cdd6cf7f14da44777f335e8582e7
Nexus0602 <124597301+Nexus0602@users.noreply.github.com>
2023-05-17 12:30:51 +0200

    Add files via upload

--- FINAL PROJECT/main.cpp
+++ FINAL PROJECT/main.cpp
@@ -1,5 +1,8 @@
 #include "Nexus.hh"
 #include <random>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
 
 int main(int argc, char *argv[]){
 
@@ -9,6 +12,7 @@ int main(int argc, char *argv[]){
     int engine_counter = 0; // controls that the speed of the objects is initialized only once, see later
     Nexus engine = Nexus(700,900);
 
+    double discountFactor = 3.5;
     string livesVec [6] = {"lives0","lives1", "lives2", "lives3", "lives4", "lives5"}; // sprite array
     vector<entt::entity> carVec; 
     int totalRails = 6;
@@ -27,7 +31,7 @@ int main(int argc, char *argv[]){
     engine.world.emplace<SpriteComponent>(frog,"frog",50,50);
     engine.world.emplace<NameGroupComponent>(frog,"player ", "player");
     engine.world.emplace<HealthComponent>(frog,5);
-    engine.world.emplace<ScoreComponent>(frog,1000);
+    engine.world.emplace<ScoreComponent>(frog,2000);
 
     /* SCORE ENTITY --> score marker */
     auto score = engine.world.create();
@@ -35,6 +39,13 @@ int main(int argc, char *argv[]){
     auto& frogscore = engine.world.get<ScoreComponent>(frog);
     std::string text = "SCORE: " + std::to_string(frogscore.score);
     engine.world.emplace<TextComponent>(score,text,color,engine.font,500,0);
+
+    /* TIMER ENTITY --> timer */
+    auto timer = engine.world.create();
+    SDL_Color color2 = {0,0,0};
+    double time = engine.GetTotalTimeInMilliSeconds();
+    std::string text2 = "TIME: 0 s " ;
+    engine.world.emplace<TextComponent>(timer,text2,color2,engine.font,275,0);
 
     /* LIVES ENTITY --> lives marker */
     auto lives = engine.world.create();
@@ -94,8 +105,10 @@ int main(int argc, char *argv[]){
 
             /* ENEMY ENTITY */
             auto enemy = engine.world.create();
-            engine.world.emplace<TransformComponent>(enemy, Vec2(dx1,512+52*i)); // velocity.y --> ensures that the vehicles appear in rails of heigth = 50
-            engine.world.emplace<KinematicComponent>(enemy); // we don't give speed yet, rendering is faster than the entity generation and the objects overlap
+            // velocity.y --> ensures that the vehicles appear in rails of heigth = 50
+            engine.world.emplace<TransformComponent>(enemy, Vec2(dx1,512+52*i)); 
+            // we don't give speed yet, rendering is faster than the entity generation and the objects overlap
+            engine.world.emplace<KinematicComponent>(enemy);
             RectangleShape r = RectangleShape(83, 43 , Color::Blue(),true);
             engine.world.emplace<ColliderComponent>(enemy, r, false, 0.3);
             engine.world.emplace<NameGroupComponent>(enemy,"enemy ", "enemies");
@@ -121,13 +134,14 @@ int main(int argc, char *argv[]){
     auto road = engine.world.create();
     engine.world.emplace<TransformComponent>(road, Vec2(width/2, 640));
     engine.world.emplace<KinematicComponent>(road);
-    RectangleShape s4 = RectangleShape(width,300, Color::Yellow(),true);
-    engine.world.emplace<ColliderComponent>(road, s4, false, 1000000);
+    //RectangleShape s4 = RectangleShape(width,300, Color::Yellow(),true);
+    //engine.world.emplace<ColliderComponent>(road, s4, false, 1000000);
     engine.world.emplace<SpriteComponent>(road,"road",width,325);
     engine.world.emplace<NameGroupComponent>(road,"road ", "road");
 
     /* RIVER MATRIX */
-    /* Instead of generating random positions, we use a matrix where 1 = waterlily, 0 = water, to ensure that the game is possible to finish */
+    /* Instead of generating random positions, we use a matrix where 1 = waterlily, 
+    0 = water, to ensure that the game is possible to finish */
     int m[5][15] = {{0, 1 ,0, 0, 0, 1, 1, 1 ,0, 0, 0, 1, 0, 0,0},
                     {1, 0 ,0, 0, 1, 1, 0, 0 ,0, 1, 0, 0, 1, 0,0},
                     {0, 0 ,0, 0, 1, 1, 1, 1 ,0, 0, 0, 1, 0, 0,1},
@@ -156,7 +170,6 @@ int main(int argc, char *argv[]){
                 else{
                     engine.world.emplace<SpriteComponent>(amigo,"nenufar2",50,50);
                 }
-                    
                 engine.world.emplace<NameGroupComponent>(amigo,"amigo ", "amigos");
                 waterlilyVec.push_back(amigo);
                 riverCounter++;
@@ -184,7 +197,7 @@ int main(int argc, char *argv[]){
     RectangleShape s5 = RectangleShape(width,300, Color::Yellow(),true);
     engine.world.emplace<ColliderComponent>(river, s5, false, 1000000);
     engine.world.emplace<SpriteComponent>(river,"river",700,255);
-    engine.world.emplace<AnimationComponent>(river,7,3.2);
+    engine.world.emplace<AnimationComponent>(river,7,4);
     engine.world.emplace<NameGroupComponent>(river,"river ", "river");
     
     
@@ -241,7 +254,9 @@ int main(int argc, char *argv[]){
 
         }
 
-        /* checking the frog's health to update the lives marker --> we don't do this part in the damage system because the lives marker can't collide, so it will never acces to that part of the code. */
+        /* checking the frog's health to update the lives marker --> 
+        we don't do this part in the damage system because the lives marker can't collide, 
+        so it will never acces to that part of the code. */
         auto& spriteA = engine.world.get<SpriteComponent>(lives);
         auto& spriteB = engine.world.get<SpriteComponent>(frog);
         auto& frogHealth =engine.world.get<HealthComponent>(frog);
@@ -251,7 +266,8 @@ int main(int argc, char *argv[]){
             spriteA.assetId = livesVec[frogHealth.health];
 
 
-        /* if the health is equals to 0, LOSE ENTITY is generated and health is changed to -1 so that it does not get looped*/
+        /* if the health is equals to 0, 
+        LOSE ENTITY is generated and health is changed to -1 so that it does not get looped*/
         if (frogHealth.health == 0){
             auto lose = engine.world.create();
             engine.world.emplace<SoundComponent>(lose,engine.loseEffect,true);
@@ -267,18 +283,32 @@ int main(int argc, char *argv[]){
         auto& text =engine.world.get<TextComponent>(score);
         text.text = "SCORE: " + std::to_string(frogscore.score);
 
+        //Updating the timer if the game is not over
+        
+        if (!frogscore.win && frogHealth.health>0){
+        auto& timertext = engine.world.get<TextComponent>(timer);
+        time = engine.GetTotalTimeInMilliSeconds();
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(0) << time;
+        std::string timeString = oss.str();
+        timertext.text = "TIME: " + timeString + " s";
+        }
+
 
         // if the frog crosses the river we will now due to its y posititon
         auto& transformFrog =engine.world.get<TransformComponent>(frog);
         if (transformFrog.position.y < 125 ){
             
             if (!frogscore.win){ // by default, win attribute is set to false
-            /* if the frog crosses the river, WIN ENTITY is generated and the win attribute is set to true, so that it does not get looped */
+            /* if the frog crosses the river, WIN ENTITY is generated and 
+            the win attribute is set to true, so that it does not get looped */
             auto win = engine.world.create();
             engine.world.emplace<SoundComponent>(win,engine.winEffect,true);
             engine.world.emplace<TransformComponent>(win, Vec2(width/2, height/2-25));
             engine.world.emplace<SpriteComponent>(win,"win",300,300);
             engine.world.remove<GridMovementComponent>(frog);
+            //calculate final score depending on the time
+            frogscore.score = frogscore.score - (time * discountFactor);
             }
             frogscore.win= true;
         }
